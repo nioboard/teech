331ec0a4fcd3456eb4e988ae6f476be1f75fd456
oshribin <oshribienhaker@gmail.com>
2015-01-06 02:24:04 +0200

    initial server

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var app = require('http');
-app.createServer(function (req, res){
-	res.writeHead(200, {'Content-Type':'text/plain'});
-	res.end('hello world');
-}).listen(1337);
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+router.get('/', function(req,res){
+	res.sendFile(__dirname + "/src/index.html");
+});
+
+
+app.use('/', router);
+app.listen(1337);
 console.log("walla");
