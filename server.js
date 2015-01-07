464cf1ba1bf7a193cb4457e062c3602209f8372d
oshribin <oshribienhaker@gmail.com>
2015-01-07 13:13:45 +0200

    login page

--- server.js
+++ server.js
@@ -1,20 +1,53 @@
 var express = require("express"),
  bodyParser = require("body-parser"),
+ cookieParser = require('cookie-parser'),
  mongoose = require('mongoose'),
  exphbs = require('express-handlebars'), 
  morgan = require('morgan'),
+ session = require('express-session'),
+ passport = require('passport'),
+ LocalStrategy = require('passport-local'),
+ mongoStore = require('connect-mongo')(session),
  app = express(),
  router = express.Router();
 
-//connect to database, teech in mongo server
+//connect to database teech in mongo server
 mongoose.connect('mongodb://localhost:27017/teech');
 
-//uing express bodyparser to parse http requests and responsw
+//using middlewares
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+	store: new mongoStore({mongooseConnection:mongoose.connection}),
+	secret: 'secretkey',
+	saveUninitialized: true,
+	resave: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(morgan('combined'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+/*app.post('/login', passport.authenticate('local-signin'),{
+	successRedirect:'/successAuthenticate',
+	failureRedirect:'/failedAuthenticate',
+});*/
+
+app.get('/failedAuthenticate', function(req,res){
+	res.send("failed");
+});
+
+app.get('/successAuthenticate', function(req,res){
+	res.send(req.user);
+});
+
+app.get('/logout', function(req,res){
+	req.logout();
+	res.redirect('/');
+});
+
 
 app.use('/lib', express.static('lib'));
 
