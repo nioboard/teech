3753c002ea019db17d1b7d304259995adef67f1e
oshribin <oshribienhaker@gmail.com>
2015-01-11 16:17:37 +0200

    facebook auth

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require("express"),
+	var express = require("express"),
  bodyParser = require("body-parser"),
  cookieParser = require('cookie-parser'),
  mongoose = require('mongoose'),
@@ -7,15 +7,17 @@ var express = require("express"),
  session = require('express-session'),
  passport = require('passport'),
  LocalStrategy = require('passport-local'),
+ FbStrategy = require('passport-facebook'),
  mongoStore = require('connect-mongo')(session),
- User = require("mongo-models/User");
+ User = require("mongo-models/User"),
+ oauthConfig = require('./oauth'),
  app = express(),
  router = express.Router();
 
 //connect to database teech in mongo server
 mongoose.connect('mongodb://localhost:27017/teech');
 
-//using middlewares
+//parsing/session/authenticate/render midlewares
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -31,68 +33,161 @@ app.use(passport.session());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.post('/login', passport.authenticate('local',{
+
+//log-in first route
+app.post('/login', passport.authenticate('local-signin',{
 	successRedirect:'/successAuthenticate',
 	failureRedirect:'/failedAuthenticate',
 	})
 );
 
+
+//registration first route
+app.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/successAuthenticate',
+  failureRedirect: '/failedAuthenticate',
+	})
+);
+
+ app.get('/auth/facebook-authentication', passport.authenticate('facebook', { scope : 'email' }));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+            successRedirect : '/',
+            failureRedirect : '/failedAuthenticate'
+        }));
+
+
+//redirect to if local-authenticate is failed
 app.get('/failedAuthenticate', function(req,res){
 	res.send("failed");
 });
 
+
+
+//redirect to if local-authentication success
 app.get('/successAuthenticate', function(req,res){
 	res.send(req.user);
 });
 
+
+//log-out route, remove the session from req and redirect to home
 app.get('/logout', function(req,res){
 	req.logout();
 	res.redirect('/');
 });
 
+
+//serialiaze the user to the session
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
- 
+
+
+//deserialize user from the session 
 passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.use(new LocalStrategy(
+
+passport.use('local-signup', new LocalStrategy(
+	function(username, password, done){
+		process.nextTick(function() {
+			User.findOne({'email':username}, function(err, user){
+				if(err)
+					return done(err)
+				if(user)
+					return done(null,false)
+				if(!user){
+					var user = new User({email:username, password:password})
+					.save(function(err, user){
+						if(err)
+							return done(err);
+						return done(null, user);
+					});
+				}
+			});
+		});
+	}
+));
+
+//local-authentication strategy
+passport.use('local-signin',new LocalStrategy(
 	function(username, password, done) {
-		console.log('-1')
 	  	process.nextTick(function() {
-	  		console.log('0');
-		  	User.findOne({'username':username}, function(err, user){
-		  		if(err){
-		  			console.log('1');
-		  			return done(err)
-		  		}
-		  		if(!user){
-		  			console.log('2');
+		  	User.findOne({'email':username}, function(err, user){
+		  		if(err)
+		  			return done(err);
+		  		if(!user)
 		  			return done(null,false);
-		  		}
-		  		if(user.password != password){
-		  			console.log('3');
+		  		if(user.password != password)
 		  			return done(null,false);
-		  		}
+		  
 		  		//make sure explicitly password is match
-		  		if(user.password == password){
-		  			console.log('4');
+		  		if(user.password == password)
 		  			return done(null,user);
-		  		}
+
 	  	});
     
     });
 }));
 
+//facebook-authentication strategy
+passport.use(new FbStrategy({
+	clientID : oauthConfig.facebook.clientID,
+	clientSecret : oauthConfig.facebook.clientSecret,
+	callbackURL : oauthConfig.facebook.callbackURL
+}, 
+	function(token, refreshToken, profile, done){
+		process.nextTick(function(){
+			User.findOne({'facebook.id': profile.id}, function(err, user){
+				if (err)
+					return done(err);
+				if(user)
+					return done(null, user);
+				if(!user){
+					//to-do: check if there is existing user with same email to avoid ambiguity
+					var user = new User({
+							'facebook':{
+								'id':profile.id,
+								'token':token,
+								'name':  profile.name.givenName + ' ' + profile.name.familyName,
+								'email': profile.emails[0].value,
+								'picture': 'https://graph.facebook.com/' + profile.id + '/picture',
+							},
+							'givenName': profile.name.givenName,
+							'familyName': profile.name.familyName,
+							'email': profile.emails[0].value,
+							'picture': 'https://graph.facebook.com/' + profile.id + '/picture',
+					});
+					user.save(function(err, user){
+						if(err)
+							return done(err);
+						return done(null, user);
+					});
+				}
+			})
+		});
+	}));
 
+
+
+
+//serve source file
 app.use('/lib', express.static(	'lib'));
 
 //serve the index file in the first 
 router.get('/', function(req,res){
 	res.sendFile(__dirname + "/lib/index.html");
 });
+
+router.post('/currentUser', function(req,res){
+	if(req.user)
+		res.send(req.user._id);
+	else
+		res.send(false);
+});
+
+
 
 router.route("/users")
 
@@ -127,7 +222,6 @@ router.route("/users")
 		})
 
 		.put(function(req, res){
-			console.log(req.body);
 			User.findById(req.params.user_id, function(err, user){
 				if(err)
 					res.send(err)
